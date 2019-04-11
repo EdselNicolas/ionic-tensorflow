efffdf3ce6aa749d16d6974edda6daad90df9ddb
EdselBNicolas <ebnicolas.halcyondigital@gmail.com>
2019-04-11 14:26:29 +0800

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Camera } from '@ionic-native/camera';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -22,6 +23,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+export class HomePage implements OnInit {
+  capturedImg: string = '';
+  recognizedResults: Array<any> = []
+  constructor(public navCtrl: NavController, private camera: Camera, private loadingCtrl: LoadingController) {
 
   }
 
+  ngOnInit() {
+
+  }
+
+  captureImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      // sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then(base64 => {
+
+      let loader = this.loadingCtrl.create({
+        content: 'Processing Image...'
+      })
+      loader.present()
+
+      let tf = new (<any>window).TensorFlow('inception-v1')
+      this.capturedImg = `data:image/jpeg;base64,${base64}`;
+      tf.classify(base64).then(results => {
+        this.recognizedResults = results
+        // results.forEach(result => {
+        //   console.log(result.title + " " + result.confidence);
+        // });
+      })
+
+
+      tf.load().then(function() {
+        loader.dismiss()
+      })
+
+   
+  }, (err) => {
+    console.log(err);
+  })
+  }
 }
